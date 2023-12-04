66128fae9ac58e532083e7e2321e0bab0b43ad3d
dnlno <daniel-nothelfer@web.de>
2023-12-04 21:18:06 +0100

    second push

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,18 @@
 <template>
-  <div id="app">
-    <div id="welcome-container">
-      <img src="../assets/logo.png" alt="HTWBook Logo" class="app-logo">
-      <input type="text" placeholder="Username">
-      <input type="password" placeholder="Password">
-      <button @click="onJoinNowClicked">SIGN IN</button>
-      <p class="caption">Connect to find and chat with your friends on HTWBook.</p>
+  <div id="app" class="container">
+    <div id="welcome-container" class="d-flex flex-column align-items-center justify-content-center min-vh-100 text-center">
+      <img src="@/assets/logo.png" alt="HTWBook Logo" class="app-logo mb-4">
+      <input type="text" class="form-control mb-3" placeholder="Benutzername">
+      <input type="password" class="form-control mb-3" placeholder="Passwort">
+      <button @click="onJoinNowClicked" class="btn btn-success btn-lg" style="max-width: 200px;">ANMELDEN</button>
+      <p class="caption mt-4">Verbinden Sie sich, um Ihre Freunde auf HTWBook zu finden und zu chatten.</p>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'WelcomePage',
+  name: 'HomeView',
   methods: {
     onJoinNowClicked () {
       this.$router.push({ name: 'dashboard' })
@@ -24,51 +24,30 @@ export default {
 <style scoped>
 @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
 
-#welcome-container {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  min-height: 100vh;
-  text-align: center;
-  font-family: SFProText-Regular, Helvetica, Arial, sans-serif;
-}
+/* Das scoped Attribut bewirkt, dass diese Stile nur auf dieses Komponenten-Template angewendet werden */
 
 .app-logo {
-  width: 120px; /* Adjusted size for larger logo */
-  margin-bottom: 2em; /* Add space below the logo */
+  width: 120px; /* Angepasste Größe für das größere Logo */
+  margin-bottom: 2em; /* Platz unter dem Logo hinzufügen */
 }
 
-#welcome-container input {
+.form-control {
   font-size: 1em;
-  padding: 12px;
-  margin: 12px 0;
-  width: 80%;
-  max-width: 320px; /* Adjusted input width */
-  border: 1px solid #ccc;
+  margin: 0.5em 0;
+  max-width: 320px; /* Angepasste Eingabefeldbreite */
   border-radius: 5px;
 }
 
-#welcome-container button {
+.btn-success {
   font-size: 1em;
-  padding: 12px 24px;
-  margin: 12px 0;
-  color: white;
-  background-color: #4CAF50; /* A green color */
-  border: none;
-  border-radius: 5px;
+  padding: 0.5em 2em;
+  margin: 0.5em 0;
   cursor: pointer;
-  transition: background-color 0.3s;
-  width: 80%;
-  max-width: 320px; /* Adjusted button width */
-}
-
-#welcome-container button:hover {
-  background-color: #45a049;
+  width: 100%; /* Button auf volle Breite des Containers ausdehnen */
 }
 
 .caption {
-  font-size: 1em; /* Larger caption for better readability */
+  font-size: 1em; /* Größere Beschriftung für bessere Lesbarkeit */
   color: #666;
   margin-top: 2em;
 }
