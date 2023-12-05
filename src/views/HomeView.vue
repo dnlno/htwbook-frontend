362682ac4227bc441598dfc398ab88a4bbcd323c
dnlno <daniel-nothelfer@web.de>
2023-12-05 23:39:08 +0100

    frontend

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,54 +1,203 @@
 <template>
-  <div id="app" class="container">
-    <div id="welcome-container" class="d-flex flex-column align-items-center justify-content-center min-vh-100 text-center">
-      <img src="@/assets/logo.png" alt="HTWBook Logo" class="app-logo mb-4">
-      <input type="text" class="form-control mb-3" placeholder="Benutzername">
-      <input type="password" class="form-control mb-3" placeholder="Passwort">
-      <button @click="onJoinNowClicked" class="btn btn-success btn-lg" style="max-width: 200px;">ANMELDEN</button>
-      <p class="caption mt-4">Verbinden Sie sich, um Ihre Freunde auf HTWBook zu finden und zu chatten.</p>
+  <div class="home-container">
+    <!-- Header-Bereich -->
+    <div class="home-header">
+      <header class="home-navbar-interactive">
+        <span class="home-logo">UNINET</span>
+        <div class="spacer"></div> <!-- Zusätzlicher Platzhalter -->
+        <div class="home-desktop-menu">
+          <button class="home-hero-button1 button">Login</button>
+          <button class="home-hero-button2 button">Register</button>
+        </div>
+      </header>
     </div>
+
+    <!-- Hero-Bereich -->
+    <div class="home-hero">
+      <div class="home-hero1">
+        <div class="home-container01">
+          <h1 class="home-hero-heading heading1">htwbook.</h1>
+          <span class="home-hero-sub-heading">CONNECT AND SHARE WITH PEOPLE</span>
+          <button class="home-learn-more-button">Learn More →</button> <!-- Button zentriert unter dem Text -->
+        </div>
+      </div>
+    </div>
+
+    <!-- Weitere Abschnitte... -->
+
+    <div class="second-page">
+      <div class="second-page-content">
+        <div class="image-box">
+          <img src="dein-bild.jpg" alt="Bild 1">
+        </div>
+        <div class="image-box">
+          <img src="dein-bild.jpg" alt="Bild 2">
+        </div>
+      </div>
+    </div>
+
+    <!-- Weitere Abschnitte... -->
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HomeView',
-  methods: {
-    onJoinNowClicked () {
-      this.$router.push({ name: 'dashboard' })
-    }
-  }
+  name: 'HomeView'
+  // Weitere Skripte...
 }
 </script>
 
 <style scoped>
-@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap');
 
-/* Das scoped Attribut bewirkt, dass diese Stile nur auf dieses Komponenten-Template angewendet werden */
-
-.app-logo {
-  width: 120px; /* Angepasste Größe für das größere Logo */
-  margin-bottom: 2em; /* Platz unter dem Logo hinzufügen */
+.home-container {
+  font-family: 'Open Sans', sans-serif;
+  background-color: #04B404; /* Grüner Hintergrund für den gesamten Container */
+  padding-top: 20px; /* Grüne Lücke am oberen Rand */
+  margin: 0;
 }
 
-.form-control {
-  font-size: 1em;
-  margin: 0.5em 0;
-  max-width: 320px; /* Angepasste Eingabefeldbreite */
-  border-radius: 5px;
+.home-container01 {
+  display: flex;           /* Flexbox-Layout aktivieren */
+  flex-direction: column;  /* Elemente vertikal anordnen */
+  align-items: center;     /* Elemente zentrieren */
+  text-align: center;      /* Text zentrieren */
 }
 
-.btn-success {
-  font-size: 1em;
-  padding: 0.5em 2em;
-  margin: 0.5em 0;
+.home-header {
+  background-color: #FFFFFF;
+  padding: 20px 40px;
+  border-radius: 38px;
+  width: 90%;
+  margin: 0 auto 30px; /* Unteren Rand beibehalten */
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 80px;
+}
+
+.home-navbar-interactive {
+  display: flex;
+  align-items: center;
+  width: 100%;
+}
+
+.home-logo {
+  color: #000000;
+  font-size: 28px;
+  font-weight: 700; /* Dickerer Schriftstil */
+}
+
+.spacer {
+  flex-grow: 1;
+}
+
+.home-desktop-menu {
+  display: flex;
+  align-items: center;
+}
+
+.home-hero-button1, .home-hero-button2 {
+  padding: 10px 20px;
+  border: none;
+  border-radius: 30px;
+  font-weight: 600;
+  transition: background-color 0.3s;
+  height: 40px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.home-hero-button1 {
+  background-color: #FFFFFF; /* Weißer Hintergrund */
+  color: #333; /* Dunkle Schriftfarbe */
+  border: 2px solid #037103; /* Grüne Outline */
+}
+
+.home-hero-button1:hover {
+  background-color: #e0e0e0; /* Hover-Effekt */
+}
+
+.home-hero-button2 {
+  background-color: #037103;
+  color: #f8f8f8; /* Weiße Schriftfarbe */
+  margin-right: 30px;
+  margin-left: 30px;
+}
+
+.home-hero-button2:hover {
+  background-color: #e0e0e0;
+}
+
+.home-hero {
+  background-color: #04B404; /* Grüner Hintergrund für den Hero-Bereich */
+  padding: 150px 0; /* Verringerte obere Polsterung */
+  text-align: center;
+}
+
+.home-hero1 {
+  color: #333;
+}
+
+.home-hero-heading {
+  color: #FFFFFF; /* Weiße Schriftfarbe */
+  font-size: 90px; /* Größere Schrift */
+  font-weight: 700; /* Dickerer Schriftstil */
+  margin-bottom: 50px; /* Verringerte untere Polsterung */
+}
+
+.home-hero-sub-heading {
+  color: #FFFFFF; /* Weiße Schriftfarbe */
+  font-size: 20px;
+  margin-bottom: 20px; /* Verringerte untere Polsterung */
+}
+
+.home-learn-more-button {
+  background-color: transparent; /* Unsichtbarer Hintergrund */
+  color: #FFFFFF;
+  border-radius: 30px;
+  border: 2px solid transparent; /* Unsichtbare Umrandung */
+  padding: 10px 20px;
+  font-size: 16px;
   cursor: pointer;
-  width: 100%; /* Button auf volle Breite des Containers ausdehnen */
+  transition: border 0.3s, color 0.3s;
+  display: inline-block; /* Ermöglicht Zentrierung */
 }
 
-.caption {
-  font-size: 1em; /* Größere Beschriftung für bessere Lesbarkeit */
-  color: #666;
-  margin-top: 2em;
+.home-learn-more-button:hover {
+  border-color: #FFFFFF; /* Weiße Umrandung beim Hover */
+  color: #FFFFFF; /* Farbwechsel beim Hover */
 }
+
+/* Weitere spezifische Styles für andere Abschnitte und Komponenten */
+
+.second-page {
+  background-color: #f2f2f2; /* Grauer Hintergrund für die zweite Seite */
+  padding: 80px 0; /* Anpassen des oberen und unteren Abstands */
+}
+
+.second-page-content {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  flex-wrap: wrap;
+  max-width: 1200px;
+  margin: 0 auto;
+}
+
+.image-box {
+  background-color: #ffffff;
+  border-radius: 10px;
+  padding: 20px;
+  margin: 20px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+
+.image-box img {
+  max-width: 100%;
+  height: auto;
+  display: block;
+}
+
 </style>
